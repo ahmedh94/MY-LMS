2233da7e8d4cc73f8403875eb851d084cf8218d6
Ahmed Hasan <134266773+ahmedh94@users.noreply.github.com>
2025-12-01 07:46:35 +0200

    day-4

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,23 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import { ReactNode } from "react";
+import logo from '@/public/LOGO.jpg';
 
 
 export default function AuthLayout({children}: {children: ReactNode}) {
     return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
+      <Link href="/" className={buttonVariants({variant: 'outline', className: 'absolute top-4 left-4'})}>
+      <ArrowLeft className="size-4"/>
+      Back
+      </Link>
         <div className="flex w-full max-w-sm flex-col gap-6">
+          <Link href="/" className="flex items-center gap-2 self-center font-medium">
+          <Image src={logo} alt="ALMS Logo" width={32} height={32} />
+          ALMS.
+          </Link>
           {children}
         </div>
     </div>
