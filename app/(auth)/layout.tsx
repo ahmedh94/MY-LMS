ed76649978ec94550c6e68a125ae46a7c7514727
Ahmed Hasan <134266773+ahmedh94@users.noreply.github.com>
2025-12-04 01:30:46 +0200

    day - 5 - lap - auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -19,6 +19,9 @@ export default function AuthLayout({children}: {children: ReactNode}) {
           ALMS.
           </Link>
           {children}
+          <div className="text-balance text-center text-xs text-muted-foreground">
+            By clicking "Continue", you agree to our <span className="hover:text-primary hover:underline">Terms of Service</span> and {" "} <span className="hover:text-primary hover:underline">Privacy Policy</span>.
+          </div>
         </div>
     </div>
 );
