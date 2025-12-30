57c8b000c4dcb62030da0411fd4785c5623514e7
Ahmed Hasan <134266773+ahmedh94@users.noreply.github.com>
2025-12-31 00:26:27 +0200

    day - 10 - lap

---
+++ lib/arcjet.ts
@@ -0,0 +1,23 @@
+import {detectBot, fixedWindow, protectSignup, sensitiveInfo, shield, slidingWindow} from '@arcjet/next'
+import arcjet from '@arcjet/next'
+import { env } from './env';
+
+export {
+ detectBot,
+ fixedWindow,
+ protectSignup,
+ sensitiveInfo,
+ shield,
+ slidingWindow,   
+}
+
+export default arcjet({
+    key: env.ARCJET_KEY,
+
+    characteristics: ["fingerprint"],
+    rules: [
+      shield({
+        mode: "LIVE",
+      })  
+    ] 
+});
